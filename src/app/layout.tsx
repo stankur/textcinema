adce4bc3858cceebf14ed33a86d7ee042ffdd471
Stanley Kurniawan <terserahskurniawan@gmail.com>
2025-07-07 22:14:06 -0700

    another attempt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         <meta name="msapplication-navbutton-color" content="#000000" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         {children}
       </body>
